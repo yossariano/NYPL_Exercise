e94a680c04f22f581b18b76d8b52e35472b68a8e
Yossarian <ianpaoconnor@gmail.com>
2023-08-10 00:00:43 -0400

    Functional random animal generation

--- app.py
+++ app.py
@@ -3,40 +3,60 @@ Main module for the NYPL Random Animal Generator.
 '''
 from flask import Flask, render_template
 
-from lib.logger import Logger
 from lib.nypldc_client import NyplDigitalCollectionClient
 from lib.nypldc_exception import NyplBackendException
+from lib.nypldc_metadata import NyplMetadata
 
-import json, sys
+import random, sys
 
 app = Flask(__name__)
 
 nypl_client = None
-logger = None
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
+# Example URL invocation:
+# http://127.0.0.1:5000/randimal/flamingo
 @app.route("/randimal/<animal>")
 def random_animal(animal):
     try:
         animal_list = nypl_client.search_for_topic(animal)
     except NyplBackendException as e:
-        logger.exception("Failed to get list of animal resouces.", e)
+        app.logger.error("Failed to get list of animal data from NYPL.", e)
         return render_template("error.html")
 
-    animal_list_count = len(animal_list)
-    logger.info("Found {count} results for '{token}'".format(count=animal_list_count, token=animal))
+    if not animal_list or len(animal_list) == 0:
+        app.logger.error("Unable to find any results for {query}".format(query=animal))
+        return render_template("404.html", value=animal)
 
-    return render_template("random_animal.html", value=animal)
+    animal_list_count = len(animal_list)
+    app.logger.info("Found {count} results for '{token}'".format(count=animal_list_count, token=animal))
+
+    # Pick a random animal from the list to display
+    random_animal_index = random.randint(0, animal_list_count - 1)
+    random_animal = animal_list[random_animal_index]
+
+    # Parse the metadata
+    random_animal_meta = NyplMetadata(random_animal)
+
+    if not random_animal_meta.image_id:
+        # Some of the entries don't have an image in them. If this is the case, just tell people to try again
+        return render_template("retry.html",
+                animal=animal,
+                item_url=random_animal_meta.item_link)
+
+    return render_template("random_animal.html",
+            animal=animal,
+            image_url=random_animal_meta.image_url,
+            item_url=random_animal_meta.item_link)
 
 if __name__ == "__main__":
-    logger = Logger()
     try:
         nypl_client = NyplDigitalCollectionClient()
     except FileNotFoundError:
-        logger.error("Failed to find auth token- cannot start. See README for more info.")
+        app.logger.error("Failed to find auth token- cannot start. See README for more info.")
         sys.exit(1)
 
     app.run()
